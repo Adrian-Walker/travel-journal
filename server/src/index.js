d0ebab00312634d228c3602a1cab01246ab23dba
Adrian Walker <awalker3365@gmail.com>
2022-03-29 16:45:23 -0400

    added mongoose

--- server/src/index.js
+++ server/src/index.js
@@ -4,16 +4,26 @@ const helmet = require('helmet')
 const mongoose = require('mongoose');
 const morgan = require('morgan')
 const middleswares = require('./middlewares');
-require('dotenv')
+require('dotenv').config();
+
 
 const app = express()
+
+mongoose.connect(`mongodb://${process.env.DATABASE_URL}`,  () => {
+    console.log('connected to db')
+});
+
+
+
+
+
 
 app.use(morgan('common'));
 
 // Hides Headers
 app.use(helmet())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN,
 }))
 
 app.get('/', (req, res) => {
