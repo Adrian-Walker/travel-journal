36249519c6d2f606b20222d4a79c755a7f2bceae
Adrian Walker <awalker3365@gmail.com>
2022-03-29 23:44:31 -0400

    updated client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Map from 'react-map-gl';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Map
+            initialViewState={{
+                longitude: -122.4,
+                latitude: 37.8,
+                zoom: 14
+            }}
+            style={{ width: 600, height: 400 }}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+        />
+    );
 }
 
-export default App;
+export default App
