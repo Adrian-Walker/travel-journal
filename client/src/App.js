c0811bd28eb28ba3e445ea7cd4cabd6ca729c6b4
Adrian Walker <awalker3365@gmail.com>
2022-04-01 15:07:32 -0400

    newer

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,40 @@
-import React, { useState } from 'react';
-import Map from 'react-map-gl';
+import React from 'react';
+import DeckGL from '@deck.gl/react';
+import { LineLayer } from '@deck.gl/layers';
+import { Map } from 'react-map-gl';
 
-function App() {
+
+
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+    longitude: -122.41669,
+    latitude: 37.7853,
+    zoom: 13,
+    pitch: 0,
+    bearing: 0
+};
+
+// Data to be used by the LineLayer
+const data = [
+    { sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781] }
+];
+
+function App({ data }) {
+    const layers = [
+        new LineLayer({ id: 'line-layer', data })
+    ];
+
     return (
-        <Map
-            initialViewState={{
-                longitude: -122.4,
-                latitude: 37.8,
-                zoom: 14
-            }}
-            style={{ width: 600, height: 400 }}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-        />
+        <DeckGL
+            initialViewState={INITIAL_VIEW_STATE}
+            controller={true}
+            layers={layers}
+        >
+            <Map mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} />
+        </DeckGL>
     );
 }
+
+
 
 export default App
