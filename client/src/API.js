eed46393e23f51c12156f3ec6838564d46090514
Adrian Walker <awalker3365@gmail.com>
2022-04-01 22:38:48 -0400

    updated front

--- client/src/API.js
+++ client/src/API.js
@@ -1,5 +1,6 @@
-const API_URL = 'httplocalhost:1337';
+const API_URL = 'http://localhost:1337';
 
-async function listLogEntries() {
-    const response = await fetch(`${API_URL}/api/logs`)
-}
+export async function listLogEntries() {
+    const response = await fetch(`${API_URL}/api/logs`);
+    return response.json();
+};
--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import Map from 'react-map-gl';
+import Map, { Marker } from 'react-map-gl';
+import { listLogEntries } from './API'
 
 
 function App() {
+    const [logEntries, setLogEntries] = useState([]);
     const [viewState, setViewState] = useState({
         longitude: -100,
         latitude: 40,
-        zoom: 15
+        zoom: 3
     });
 
     useEffect(() => {
-
+        (async () => {
+            const logEntries = await listLogEntries();
+            setLogEntries(logEntries)
+        })();
     }, []);
 
     return <Map
@@ -19,6 +24,19 @@ function App() {
         mapStyle="mapbox://styles/1wayoranother/cl1h4gn5t001g15nq1z4gcqa0"
         style={{ width: "95vw", height: "100vh", margin: "auto" }}
         mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-    />;
+    >
+        {
+            logEntries.map(entry => {
+                <Marker
+                    key={entry._id}
+                    longitude={entry.longitude}
+                    latitude={entry.latitude}
+                    anchor="bottom" >
+                    <img src="./pin.png" />
+                    <div>{entry.title}</div>
+                </Marker>
+            })
+        }
+    </Map>
 }
 export default App
