095b3447f20a804857ef123769404371551c63b9
Adrian Walker <awalker3365@gmail.com>
2022-04-03 00:00:03 -0400

    updated frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import Map, { Marker } from 'react-map-gl';
+import Map, { Marker, Popup } from 'react-map-gl';
 import { listLogEntries } from './API'
 
 
 function App() {
     const [logEntries, setLogEntries] = useState([]);
+    const [showPopup, setShowPopup] = useEffect({})
     const [viewState, setViewState] = useState({
         longitude: -100,
         latitude: 40,
@@ -26,16 +27,58 @@ function App() {
         mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
     >
         {
-            logEntries.map(entry => {
-                <Marker
-                    key={entry._id}
-                    longitude={entry.longitude}
-                    latitude={entry.latitude}
-                    anchor="bottom" >
-                    <img src="./pin.png" />
-                    <div>{entry.title}</div>
-                </Marker>
-            })
+            logEntries.map(entry => (
+                <div>
+                    <Marker
+                        key={entry._id}
+                        longitude={entry.longitude}
+                        latitude={entry.latitude}
+                        anchor="bottom" >
+                        <div
+                            onClick={() => setShowPopup({
+                                // ...showPopup,
+                                [entry._id]: true,
+                            })}
+                        >
+                            <svg
+                                viewBox="0 0 24 24"
+                                style={{
+                                    width: '10px',
+                                    height: '10px',
+                                }}
+                                width="52" height="52"
+                                stroke="red"
+                                className='marker'
+                                stroke-width="1.5"
+                                fill="none"
+                                stroke-linecap="round"
+                                stroke-linejoin="round" >
+                                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                                <circle cx="12" cy="10" r="3"></circle>
+                            </svg>
+                        </div>
+                    </Marker>
+                    {
+                        showPopup[entry._id] ? (
+                            <Popup
+                                latitude={entry.latitude}
+                                longitude={entry.longitude}
+                                closeButton={true}
+                                closeOnClick={false}
+                                dynamicPosition={true}
+                                onClose={() => setShowPopup({})}
+                                anchor="top" >
+                                <div className="popup">
+                                    <h3>{entry.title}</h3>
+                                    <p>{entry.comments}</p>
+                                    <small>Visited on: {new Date(entry.visitDate).toLocaleDateString()}</small>
+                                    {entry.image && <img src={entry.image} alt={entry.title} />}
+                                </div>
+                            </Popup>
+                        ) : null
+                    }
+                </div>
+            ))
         }
     </Map>
 }
